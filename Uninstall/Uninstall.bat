004bd04010feb4f58040a92d043c80d589f63ee5
SteveWyntontje <wyntontjes123@outlook.com>
2024-11-17 17:30:47 +0100

    made it better

--- Uninstall/Uninstall.bat
+++ Uninstall/Uninstall.bat
@@ -1,30 +1,31 @@
 @echo off
 echo Requesting the deletion of VirtualBox... Please wait...
 :Delete VBox
-set /p Delete VBox=Do you want to delete VirtualBox? (y/n)
+set /p Delete_VBox=Do you want to delete VirtualBox? (y/n)
 IF %Delete VBox% == "y" (
-taskkill /F /IM "%PROGRAMFILES%\Oracle\VirtualBox\*.exe"
-MsiExec.exe /I{95DEBF01-7029-4E37-BDB1-94EFEA3B263C} ||
+taskkill /IM VBox*.exe
+taskkill /IM VirtualBox*.exe
+MsiExec.exe /I{95DEBF01-7029-4E37-BDB1-94EFEA3B263C}
 )
-IF %Delete VBox% == "n" (
+IF %Delete_VBox% == "n" (
 timeout /t 1 /nobreak >nul
 )
-IF %Delete VBox% == "" (
+IF %Delete_VBox% == "" (
 echo Please give an input. 
 goto Delete VBox
 )
 echo Finished. Now requesting the deletion of the VM...
 :Delete VM
-set /p Delete VM=Do you want to delete Windows 3.1? (y/n)
-IF %Delete VM% == "y" (
+set /p Delete_VM=Do you want to delete Windows 3.1? (y/n)
+IF %Delete_VM% == "y" (
 RMDIR /S /Q "%USERPROFILE%\Virtualbox VMs\Windows 3.1"
 RMDIR /S /Q "%USERPROFILE%\Uninstall Windows 3.1"
 RMDIR /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows 3.1"
 )
-IF %Delete VM% == "n" (
+IF %Delete_VM% == "n" (
 timeout /t 1 /nobreak >nul
 )
-IF %Delete VM% == "" (
+IF %Delete_VM% == "" (
 echo Please give an input. 
 goto Delete VM
 )
